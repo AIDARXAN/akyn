cb9a20542916476b459d1af15be725d01cdc4fbe
Aidar <ajdarbekzamirbekov@gmail.com>
2021-02-04 16:33:17 +0600

    ADDED Register serializer for registration.

--- backend/users_api/serializers.py
+++ backend/users_api/serializers.py
@@ -1,4 +1,8 @@
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+from rest_auth.registration.serializers import RegisterSerializer
 from rest_auth.serializers import LoginSerializer
+from rest_framework import serializers
 
 
 class CustomLoginSerializer(LoginSerializer):
@@ -6,3 +10,32 @@ class CustomLoginSerializer(LoginSerializer):
 
     class Meta:
         fields = ['email', 'password']
+
+
+class RegisterSerializer(RegisterSerializer):
+    phone = serializers.CharField(max_length=12, write_only=True)
+    first_name = serializers.CharField(max_length=255, write_only=True)
+    last_name = serializers.CharField(max_length=255, write_only=True)
+
+    def validate_phone(self, value):
+        return get_adapter().clean_phone(value)
+
+    def get_cleaned_data(self):
+        return {
+            'password1': self.validated_data.get('password1', ''),
+            'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
+            'phone': self.validated_data.get('phone', ''),
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        adapter.save_user(request, user, self)
+        setup_user_email(request, user, [])
+        return user
+
+    class Meta:
+        ref_name = 'custom register serializer'
