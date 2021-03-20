2994567ae54fdab44dc589379f592f5720f85e02
Aidar <ajdarbekzamirbekov@gmail.com>
2021-03-20 23:36:06 +0600

    DELETED unused files

--- backend/feed_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeedApiConfig(AppConfig):
-    name = 'feed_api'
--- frontend/src/components/Calendar/calendarSevice.js
+++
@@ -1,8 +0,0 @@
-
-export const getDayOfWeak = date => {
-    const dayIfWeek = date.getDay();
-
-    if (dayIfWeek === 0) return 6;
-
-    return dayIfWeek - 1;
-};
