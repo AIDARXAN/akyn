cbf5eb4c57c05eb7a7ab6b6340194ca99fdc28ba
Aidar <ajdarbekzamirbekov@gmail.com>
2021-03-31 10:46:22 +0600

    ADDED url path for password reset and confirm.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
+from api.api_views.password_reset import PasswordResetView, PasswordResetConfirmView
 from api.views import CustomRegisterView, ProfileView
 
 urlpatterns = [
     path('registration/', CustomRegisterView.as_view(), name='custom_rest_register'),
-    path('users/current', ProfileView.as_view(), name='user_profile')
+    path('users/current/', ProfileView.as_view(), name='user_profile'),
+    path('password/reset/', PasswordResetView.as_view(), name='custom_password_rest_reset'),
+    path(
+        'password/reset/confirm/<str:reset_token>/',
+        PasswordResetConfirmView.as_view(),
+        name='custom_password_rest_reset_confirm'
+    ),
 ]
