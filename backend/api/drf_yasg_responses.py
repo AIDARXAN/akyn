e47fa27c4b834e92fcf5965b1fbbeb0b5baa938a
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-04 22:43:33 +0600

    ADDED drf responses

---
+++ backend/api/drf_yasg_responses.py
@@ -0,0 +1,6 @@
+from drf_yasg import openapi
+
+bad_request = openapi.Response(description='No_content')
+not_found = openapi.Response(description='Not Found')
+success_response_ok = openapi.Response(description="Ok")
+
