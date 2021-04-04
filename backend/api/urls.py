310d1571a5aeb4f1494cf3a677badaf8b4257642
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-04 22:46:54 +0600

    ADDED endpoint for returning user profile for another user.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from api.api_views.password_reset import PasswordResetView, PasswordResetConfirmView
-from api.views import CustomRegisterView, ProfileView
+from api.views import CustomRegisterView, ProfileView, ProfileViewForThirdUser
 
 urlpatterns = [
     path('registration/', CustomRegisterView.as_view(), name='custom_rest_register'),
     path('users/current/', ProfileView.as_view(), name='user_profile'),
     path('password/reset/', PasswordResetView.as_view(), name='custom_password_rest_reset'),
+    path('users/<str:username>/', ProfileViewForThirdUser.as_view(), name='profile_view_for_third_party'),
     path(
         'password/reset/confirm/<str:reset_token>/',
         PasswordResetConfirmView.as_view(),
