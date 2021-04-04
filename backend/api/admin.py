e432a7d34cdc9ca0acae9b863f0e5b050ca2c64e
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-04 22:42:58 +0600

    REGISTERED Publication model in the admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import User, Follow
+from api.models import User, Follow, Publication
 
 admin.site.register(User)
 admin.site.register(Follow)
+admin.site.register(Publication)
