608e7e69fc73953094e34f7a7523e711f182d832
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-07 22:40:59 +0600

    REMOVED auth from path api.urls.

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
-    path('api/v1/auth/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
     path('allauth/', include('allauth.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
