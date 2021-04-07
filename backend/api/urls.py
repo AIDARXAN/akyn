de719ff5415ad020ebf6aa24146b8f59f590e479
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-07 22:40:38 +0600

    ADDED url paths for publicaitons.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 
 from api.api_views.password_reset import PasswordResetView, PasswordResetConfirmView
-from api.views import CustomRegisterView, ProfileView, ProfileViewForThirdUser
+from api.views import CustomRegisterView, ProfileView, ProfileViewForThirdUser, PublicationDetailView, PublicationView
 
 urlpatterns = [
-    path('registration/', CustomRegisterView.as_view(), name='custom_rest_register'),
-    path('users/current/', ProfileView.as_view(), name='user_profile'),
-    path('password/reset/', PasswordResetView.as_view(), name='custom_password_rest_reset'),
-    path('users/<str:username>/', ProfileViewForThirdUser.as_view(), name='profile_view_for_third_party'),
+    path('auth/registration/', CustomRegisterView.as_view(), name='custom_rest_register'),
+    path('auth/password/reset/', PasswordResetView.as_view(), name='custom_password_rest_reset'),
     path(
-        'password/reset/confirm/<str:reset_token>/',
+        'auth/password/reset/confirm/<str:reset_token>/',
         PasswordResetConfirmView.as_view(),
         name='custom_password_rest_reset_confirm'
     ),
+
+    path('users/current/', ProfileView.as_view(), name='user_profile'),
+    path('users/<str:username>/', ProfileViewForThirdUser.as_view(), name='profile_view_for_third_party'),
+    # path('users/search/<str:search>/'),
+
+    path('publications/', PublicationView.as_view(), name='publications'),
+    path('publications/<int:publication_pk>/', PublicationDetailView.as_view(), name='publication_detail'),
 ]
