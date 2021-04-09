ead3f4cc58e61e4ecd953ab2c59e8fae19e311c7
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-09 12:09:14 +0600

    ADDED Comment to the admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import User, Follow, Publication
+from api.models import User, Follow, Publication, Comment
 
 admin.site.register(User)
 admin.site.register(Follow)
 admin.site.register(Publication)
+admin.site.register(Comment)
