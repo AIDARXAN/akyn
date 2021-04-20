c3f1be70c562eab87eeed7245f8fbe4a65897f9f
Aidar <ajdarbekzamirbekov@gmail.com>
2021-04-20 14:52:54 +0600

    ADDED navlinkgs to user profile

--- frontend/src/components/Comment/Comment.js
+++ frontend/src/components/Comment/Comment.js
@@ -4,13 +4,17 @@ import moment from "moment";
 import {urlWithoutApiPrefix} from "../../configAPI";
 import "../Post/post.css";
 import "./comments.css";
+import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
+import "../Post/post.css";
 
 const Comment = ({comment}) => {
+    const currentUser = useSelector(state => state.user.currentUser)
     return (
         <CardBody className="pt-0 pb-0">
             <hr/>
             <div className="row" style={{height: '25px' }}>
-                <div className="col-6">{comment?.user.first_name} {comment?.user.last_name}</div>
+                <NavLink to={!comment.is_owner ? '/profile/'+ comment?.user.username : '/profile'} className="col-6 nav-link-font">{comment?.user.first_name} {comment?.user.last_name}</NavLink>
                 <div className={"col-2"}>{moment(comment?.creation_date).format("DD.MM.YYYY")}</div>
                 {comment.is_owner && comment.changed && <div className={"col-2"}>(өзгөртүлдү)</div>}
                 {comment.is_owner && <div className="col-2 offset-2"><img className="comment-more-icon"
--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -11,6 +11,7 @@ import CommentCreateForm from "./CommentCreateForm";
 import {openSuccessAlert} from "../../store/acrions/Notification/actionCreators";
 import CommentsListForm from "./CommentsListForm";
 import {fetchComments} from "../../store/acrions/Comments/actionCreators";
+import {NavLink} from "react-router-dom";
 
 const Post = ({post}) => {
     const dispatch = useDispatch();
@@ -63,13 +64,17 @@ const Post = ({post}) => {
                             className="avatar border-gray ml-3"
                             src={post.user.avatar ? post.user.avatar : require("assets/img/cat.jpeg")}
                         />
-                        <div className="col-4">{post?.user.first_name} {post?.user.last_name}</div>
-                        <div className={"col-2"}>@{post?.user.username}</div>
+                        <NavLink to={post.is_owner ? "/profile" : "/profile/" + post?.user.username}
+                                 className="col-4 nav-link-font">{post?.user.first_name} {post?.user.last_name}</NavLink>
+                        <NavLink to={post.is_owner ? "/profile" : "/profile/" + post?.user.username}
+                                 className="col-2 nav-link-font">@{post?.user.username}</NavLink>
                         <div className={"col-4"}>{moment(post?.creation_date).format("DD.MM.YYYY")}</div>
-                        {post.is_owner && <div className="col-1 float-right"><img className="post-more-icon" src={require('assets/icons/more.svg')} alt=""/></div>}
-                         {/*TODO drop down*/}
+                        {post.is_owner && <div className="col-1 float-right"><img className="post-more-icon"
+                                                                                  src={require('assets/icons/more.svg')}
+                                                                                  alt=""/></div>}
+                        {/*TODO drop down*/}
                     </div>
-                    <pre className="post-description ml-5">
+                    <pre className="post-description ml-5 mt-3">
                     {post?.description}
                 </pre>
                     <div className="row">
@@ -77,8 +82,10 @@ const Post = ({post}) => {
                              src={postIsLiked ? require("assets/icons/liked.svg") : require("assets/icons/like.svg")}
                              alt=""/>
                         <p className="post-like-number ml-2">{postLikes}</p>
-                        <i className="nc-icon nc-chat-33 ml-4 post-cursor-pointer" style={{fontSize: "25px"}} onClick={toggleCommentModal}/>
-                        <a className="post-like-comment ml-2 post-cursor-pointer" onClick={toggleCommentsListModal}>{postComments}</a>
+                        <i className="nc-icon nc-chat-33 ml-4 post-cursor-pointer" style={{fontSize: "25px"}}
+                           onClick={toggleCommentModal}/>
+                        <a className="post-like-comment ml-2 post-cursor-pointer"
+                           onClick={toggleCommentsListModal}>{postComments}</a>
                     </div>
 
                 </CardBody>
@@ -86,7 +93,8 @@ const Post = ({post}) => {
             <Modal isOpen={commentModal} toggle={toggleCommentModal} centered={true}>
                 <CommentCreateForm commentSubmit={commentSubmit}/>
             </Modal>
-            {comments && <Modal isOpen={commentsListModal} toggle={toggleCommentsListModal} centered={true} style={{maxHeight: "80%"}}>
+            {comments && <Modal isOpen={commentsListModal} toggle={toggleCommentsListModal} centered={true}
+                                style={{maxHeight: "80%"}}>
                 <CommentsListForm commentSubmit={commentSubmit} comments={comments} style={{maxHeight: "80%"}}/>
             </Modal>}
         </>
